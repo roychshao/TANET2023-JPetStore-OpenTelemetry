a43b0c3ab715d84c3c3696cd3ba2f118fff216ac
roychshao <roych.shao@gmail.com>
2023-02-19 05:32:48 +0800

    add: solve table not exist, but connection failure

--- k8s_config/deploy.sh
+++ k8s_config/deploy.sh
@@ -1,3 +1,4 @@
+kubectl create configmap init-script --from-file=01-schema.sql --from-file=02-dataload.sql
 kubectl apply -f jpetstore-configmap.yaml
 kubectl apply -f jpetstore-secret.yaml
 kubectl apply -f jpetstore-database.yaml
