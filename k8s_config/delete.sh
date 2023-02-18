807f1f1e85c415966c5c81d65cb2e5dc87f2a3f6
roychshao <roych.shao@gmail.com>
2023-02-19 00:17:16 +0800

    reconstruct: attempt to run on minikube

---
+++ k8s_config/delete.sh
@@ -0,0 +1,6 @@
+kubectl delete deployment jpetstore-main
+kubectl delete statefulset jpetstore-database
+kubectl delete service database-service
+kubectl delete service main-service
+kubectl delete configmap jpetstore-configmap
+kubectl delete secret jpetstore-secret
---
+++ k8s_config/deploy.sh
@@ -0,0 +1,4 @@
+kubectl apply -f jpetstore-configmap.yaml
+kubectl apply -f jpetstore-secret.yaml
+kubectl apply -f jpetstore-database.yaml
+kubectl apply -f jpetstore-main.yaml
